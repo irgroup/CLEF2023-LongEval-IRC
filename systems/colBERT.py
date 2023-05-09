702b6f7ebf25cad060fd7795c0dd4b6703437cfc
jueri <jueri.keller@smail.th-koeln.de>
2023-05-09 09:34:00 +0200

    add colBERT system

---
+++ systems/colBERT.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""System that converts the documents into passages and uses BM25 to rank them.
+
+Example:
+    Run the system with the following command::
+
+        $ python -m systems.BM25_passage --index WT
+"""
+from argparse import ArgumentParser
+from src.exp_logger import logger
+
+import pyterrier as pt  # type: ignore
+
+from src.load_index import setup_system, tag
+import yaml
+import os
+
+from pyterrier_colbert.ranking import ColBERTFactory
+
+with open("settings.yml", "r") as yamlfile:
+    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+
+
+def get_system() -> pt.BatchRetrieve:
+    """Return the system as a pyterrier BatchRetrieve object.
+
+    Returns:
+        pt.BatchRetrieve: System as a pyterrier BatchRetrieve object.
+    """
+    pytcolbert = ColBERTFactory("../data/models/colbert.dnn", "../data/index/index_WT_colBERT", "WT_colbert", faiss_partitions=100)
+    
+    dense_e2e = pytcolbert.end_to_end()
+
+    return dense_e2e
+
+
+def main():
+    run_tag = tag("BM25", "colBERT")
+
+    topics = pt.io.read_topics("../" + config["WT"]["train"]["topics"])
+
+    system = get_system()
+    results = system.transform(topics)
+
+    pt.io.write_results(res=results, filename=os.path.join("..", config["results_path"] + run_tag))
+
+
+if __name__ == "__main__":
+
+    main()
