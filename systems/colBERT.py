f47bca72ca56bfaf9662d07fca50ed5e3065286c
jueri <jueri.keller@smail.th-koeln.de>
2023-05-12 10:42:27 +0200

    update colBERT dense system with metadata

--- systems/colBERT.py
+++ systems/colBERT.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""System that converts the documents into passages and uses BM25 to rank them.
+"""ColBER dense retrieval system.
 
 Example:
-    Run the system with the following command::
+    Create runs on the train topics of the given index::
 
-        $ python -m systems.BM25_passage --index WT
+        $ python -m systems.colBERT --index WT_colBERT --train
+
+    Create runs on the test topics of the given index::
+
+        $ python -m systems.colBERT --index WT_colBERT
+    
 """
 from argparse import ArgumentParser
 from src.exp_logger import logger
 
 import pyterrier as pt  # type: ignore
+from src.metadata import get_metadata, write_metadata_yaml
 
 from src.load_index import setup_system, tag
 import yaml
@@ -22,29 +28,66 @@ with open("settings.yml", "r") as yamlfile:
     config = yaml.load(yamlfile, Loader=yaml.FullLoader)
 
 
-def get_system() -> pt.BatchRetrieve:
+def get_system(index_path, index) -> pt.BatchRetrieve:
     """Return the system as a pyterrier BatchRetrieve object.
 
     Returns:
         pt.BatchRetrieve: System as a pyterrier BatchRetrieve object.
     """
-    pytcolbert = ColBERTFactory("data/models/colbert.dnn", "data/index/index_WT_colBERT", "WT_colbert", faiss_partitions=100, gpu=False)
+    pytcolbert = ColBERTFactory("data/models/colbert.dnn", index_path, index, faiss_partitions=100, gpu=False)
     dense_e2e = pytcolbert.end_to_end() % 1000
 
     return dense_e2e
 
 
-def main():
-    run_tag = tag("colBERT", "WT")
+def main(args):
+    run_tag = tag(args.index[3:], args.index)
+    checkpoint = f"intfloat/{args.index[3:]}"
 
-    topics = pt.io.read_topics(config["WT"]["train"]["topics"])
-
-    system = get_system()
+    slice = "train" if args.train else "test"
+    topics = pt.io.read_topics(config[args.index][slice]["topics"])
+    index_path = os.path.join(config["index_dir"], args.index)
+    
+    system = get_system(index_path=index_path, index=args.index)
     results = system.transform(topics)
 
-    pt.io.write_results(res=results, filename= config["results_path"] + run_tag)
+    pt.io.write_results(res=results, filename=config["results_path"] + run_tag)
 
+    write_metadata_yaml(
+            config["metadata_path"] + run_tag + ".yml",
+            {
+                "tag": run_tag,
+                "method": {
+                    "indexing": {
+                        "colBERT": {
+                            "method": "pyterrier_colbert.indexing.ColBERTIndexer",
+                            "checkpoint": "http://www.dcs.gla.ac.uk/~craigm/colbert.dnn.zip",
+                        }
+                    },
+                    "retrieval": {
+                        "1": {
+                            "name": "colBERT",
+                            "method": "pyterrier_colbert.ranking.ColBERTFactory.end_to_end",
+                        }
+                    },
+                },
+            },
+        )
 
 if __name__ == "__main__":
+    parser = ArgumentParser(description="Create an pyterrier index from a config.")
+    parser.add_argument(
+        "--index",
+        type=str,
+        required=True,
+        help="Name of the dataset in the config file (WT, ST or LT)",
+    )
+    parser.add_argument(
+        "--train",
+        required=False,
+        action="store_true",
+        help="Use the train topics to create the.",
+    )
 
-    main()
+    args = parser.parse_args()
+    main(args)
